819d14352d078ccfe7c068f9e0eefa7d545fa8de
Filip Hracek <filip.hracek@gmail.com>
2020-03-13 10:58:55 -0700

    Add more precision levels

--- lib/src/formatter.dart
+++ lib/src/formatter.dart
@@ -4,6 +4,21 @@ import 'dart:math';
 /// top to bottom, and tries each precision until it finds a good match.
 final _precisions = <_Precision>[
   _Precision(
+    '350B',
+    (n) => _quantize(n / 1000000000, 50) + 'B',
+    (s) => double.tryParse(s.substring(0, s.length - 1)) * 1000000000,
+  ),
+  _Precision(
+    '340B',
+    (n) => _quantize(n / 1000000000, 10) + 'B',
+    (s) => double.tryParse(s.substring(0, s.length - 1)) * 1000000000,
+  ),
+  _Precision(
+    '345B',
+    (n) => _quantize(n / 1000000000, 5) + 'B',
+    (s) => double.tryParse(s.substring(0, s.length - 1)) * 1000000000,
+  ),
+  _Precision(
     '5B',
     (n) => _divide(n, 1000000000) + 'B',
     (s) => double.tryParse(s.substring(0, s.length - 1)) * 1000000000,
@@ -12,6 +27,21 @@ final _precisions = <_Precision>[
     '4.2B',
     (n) => (n / 1000000000).toStringAsFixed(1) + 'B',
     (s) => double.tryParse(s.substring(0, s.length - 1)) * 1000000000,
+  ),
+  _Precision(
+    '350M',
+    (n) => _quantize(n / 1000000, 50) + 'M',
+    (s) => double.tryParse(s.substring(0, s.length - 1)) * 1000000,
+  ),
+  _Precision(
+    '340M',
+    (n) => _quantize(n / 1000000, 10) + 'M',
+    (s) => double.tryParse(s.substring(0, s.length - 1)) * 1000000,
+  ),
+  _Precision(
+    '345M',
+    (n) => _quantize(n / 1000000, 5) + 'M',
+    (s) => double.tryParse(s.substring(0, s.length - 1)) * 1000000,
   ),
   _Precision(
     '5M',
@@ -24,7 +54,22 @@ final _precisions = <_Precision>[
     (s) => double.tryParse(s.substring(0, s.length - 1)) * 1000000,
   ),
   _Precision(
-    '52K',
+    '350K',
+    (n) => _quantize(n / 1000, 50) + 'K',
+    (s) => double.tryParse(s.substring(0, s.length - 1)) * 1000,
+  ),
+  _Precision(
+    '340K',
+    (n) => _quantize(n / 1000, 10) + 'K',
+    (s) => double.tryParse(s.substring(0, s.length - 1)) * 1000,
+  ),
+  _Precision(
+    '345K',
+    (n) => _quantize(n / 1000, 5) + 'K',
+    (s) => double.tryParse(s.substring(0, s.length - 1)) * 1000,
+  ),
+  _Precision(
+    '343K',
     (n) => _divide(n, 1000) + 'K',
     (s) => double.tryParse(s.substring(0, s.length - 1)) * 1000,
   ),
