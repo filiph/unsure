bd27de285320530e654159fea941fd5242833b8b
Filip Hracek <filip.hracek@gmail.com>
2020-03-12 23:26:43 -0700

    Make a more usable exacutable

--- bin/unsure.dart
+++ bin/unsure.dart
@@ -1,62 +1,42 @@
 import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
 
+import 'package:args/args.dart';
 import 'package:unsure/src/calculation.dart';
 import 'package:unsure/src/parser.dart';
 
 Future<int> main(List<String> args) async {
-  // TODO: use package:args for this
-  if (args.contains('help') ||
-      args.contains('-?') ||
-      args.contains('-h') ||
-      args.contains('--help')) {
+  final argParser = ArgParser();
+
+  argParser
+    ..addFlag('help',
+        abbr: 'h', help: 'Print this help information.', negatable: false)
+    ..addFlag('histogram', help: 'Show the histogram.', defaultsTo: true);
+
+  final argResults = argParser.parse(args);
+
+  if (argResults['help']) {
     print(_help);
+    print(argParser.usage);
     return 2;
   }
-  final machine = args.contains('--machine');
 
-  if (stdout.hasTerminal) {
-    stdout.writeln('Write a formula and hit enter. To end the session, '
-        'type exit or hit Ctrl-D.');
-    stdout.write('> ');
-  }
+  final formulaString = argResults.rest.join(' ');
 
-  final lineSplitter = LineSplitter();
-  var hintGiven = false;
-
-  final lines =
-      await stdin.timeout(const Duration(milliseconds: 500), onTimeout: (sink) {
-    if (stdin.hasTerminal || hintGiven) return;
-    stderr.writeln('Nothing has come from this pipe for more than 500ms. '
-        'Is everything set up properly?');
-    hintGiven = true;
-  }).map((bytes) {
-    final string = utf8.decode(bytes);
-    final lines = lineSplitter.convert(string);
-    return lines;
-  }).expand((lines) => lines);
-
-  // TODO wrap with keyboard interrupt try-catch
-  await for (final line in lines) {
-    if (line.trim() == 'exit' || line.trim() == 'quit') {
-      break;
-    }
-
-    final formula = parseString(line);
-    final calculation = Calculation(formula);
-    final result = calculation.run();
-    stdout.writeln(result.simple);
-    stdout.writeln(result.histogram);
-
-    if (stdout.hasTerminal) {
-      stdout.write('> ');
-    }
+  final formula = parseString(formulaString);
+  final calculation = Calculation(formula);
+  final result = calculation.run();
+  print(result.simple);
+  if (argResults['histogram']) {
+    print('\nHistogram:\n');
+    print(result.histogram);
   }
 
   return 0;
 }
 
 const _help = r'''
-Just run this and start typing formulas.
+Run this executable with the formula you're trying to compute. For example:
+
+  $ unsure 4~6 * 1~2
+
 ''';
